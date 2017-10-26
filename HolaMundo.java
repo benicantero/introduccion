9810f4f288ee6d7c874ae3b6d5d70a4af0b007fb
Beni <[email>
2017-10-26 14:43:42 +0200

    fist commit

---
+++ HolaMundo.java
@@ -0,0 +1,13 @@
+/**public class Clase1{
+	public static void main(String arg[]){
+		System.out.println("Clase1");
+		}
+}
+*/
+    public class HolaMundo {
+     
+    	public static void main(String[] args) {		
+    		System.out.println("Hola Mundo");
+    	}
+     
+    }
